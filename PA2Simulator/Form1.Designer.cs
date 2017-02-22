4a444940b0d74839c014b8d67c10d09362d4d135
Butch <butchtabios@live.com>
2017-02-22 14:22:53 +0800

    initial

---
+++ PA2Simulator/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace PA2Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.Vrms_textBox = new System.Windows.Forms.TextBox();
+            this.Irms_textBox = new System.Windows.Forms.TextBox();
+            this.Preal_textBox = new System.Windows.Forms.TextBox();
+            this.PF_textBox = new System.Windows.Forms.TextBox();
+            this.Watthr_textBox = new System.Windows.Forms.TextBox();
+            this.Interval_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PA2Test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.FormattingEnabled = true;
+            this.PortComboBox.Location = new System.Drawing.Point(41, 45);
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(114, 24);
+            this.PortComboBox.TabIndex = 3;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(212, 38);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(108, 36);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // Vrms_textBox
+            // 
+            this.Vrms_textBox.Location = new System.Drawing.Point(41, 115);
+            this.Vrms_textBox.Name = "Vrms_textBox";
+            this.Vrms_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Vrms_textBox.TabIndex = 6;
+            this.Vrms_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Irms_textBox
+            // 
+            this.Irms_textBox.Location = new System.Drawing.Point(212, 115);
+            this.Irms_textBox.Name = "Irms_textBox";
+            this.Irms_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Irms_textBox.TabIndex = 7;
+            this.Irms_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Preal_textBox
+            // 
+            this.Preal_textBox.Location = new System.Drawing.Point(377, 115);
+            this.Preal_textBox.Name = "Preal_textBox";
+            this.Preal_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Preal_textBox.TabIndex = 8;
+            this.Preal_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PF_textBox
+            // 
+            this.PF_textBox.Location = new System.Drawing.Point(41, 171);
+            this.PF_textBox.Name = "PF_textBox";
+            this.PF_textBox.Size = new System.Drawing.Size(100, 22);
+            this.PF_textBox.TabIndex = 9;
+            this.PF_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Watthr_textBox
+            // 
+            this.Watthr_textBox.Location = new System.Drawing.Point(212, 171);
+            this.Watthr_textBox.Name = "Watthr_textBox";
+            this.Watthr_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Watthr_textBox.TabIndex = 10;
+            this.Watthr_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Interval_textBox
+            // 
+            this.Interval_textBox.Location = new System.Drawing.Point(377, 171);
+            this.Interval_textBox.Name = "Interval_textBox";
+            this.Interval_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Interval_textBox.TabIndex = 11;
+            this.Interval_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Vrms";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Irms";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Preal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "P.F.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Watt-Hr.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(410, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Time";
+            // 
+            // PA2Test
+            // 
+            this.PA2Test.Location = new System.Drawing.Point(377, 38);
+            this.PA2Test.Name = "PA2Test";
+            this.PA2Test.Size = new System.Drawing.Size(108, 36);
+            this.PA2Test.TabIndex = 18;
+            this.PA2Test.Text = "PA2 OFF";
+            this.PA2Test.UseVisualStyleBackColor = true;
+            this.PA2Test.Click += new System.EventHandler(this.PA2Test_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 253);
+            this.Controls.Add(this.PA2Test);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Interval_textBox);
+            this.Controls.Add(this.Watthr_textBox);
+            this.Controls.Add(this.PF_textBox);
+            this.Controls.Add(this.Preal_textBox);
+            this.Controls.Add(this.Irms_textBox);
+            this.Controls.Add(this.Vrms_textBox);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.PortComboBox);
+            this.Name = "Form1";
+            this.Text = "PA2 Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox Vrms_textBox;
+        private System.Windows.Forms.TextBox Irms_textBox;
+        private System.Windows.Forms.TextBox Preal_textBox;
+        private System.Windows.Forms.TextBox PF_textBox;
+        private System.Windows.Forms.TextBox Watthr_textBox;
+        private System.Windows.Forms.TextBox Interval_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button PA2Test;
+    }
+}
+
---
+++ PA2Simulator/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace PA2Simulator
+{
+    public partial class Form1 : Form
+    {
+        private static System.Timers.Timer aTimer; 
+
+        SerialPort _serial_port = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            GetSerialPorts();
+
+            // Create a timer and set a one second interval.
+            aTimer = new System.Timers.Timer(1000);
+
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+
+            // Have the timer fire repeated events (true is the default)
+            aTimer.AutoReset = true;
+
+            // Start the timer
+            aTimer.Enabled = true;
+        }
+
+        private void GetSerialPorts()
+        {
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+
+            ArrayComPortsNames = SerialPort.GetPortNames();
+
+            do
+            {
+                index += 1;
+                PortComboBox.Items.Add(ArrayComPortsNames[index]);
+            } while (!((ArrayComPortsNames[index] == ComPortName) ||
+                       (index == ArrayComPortsNames.GetUpperBound(0))));
+
+            Array.Sort(ArrayComPortsNames);
+
+            //want to get first out
+            if (index == ArrayComPortsNames.GetUpperBound(0))
+            {
+                ComPortName = ArrayComPortsNames[0];
+            }
+            PortComboBox.Text = ArrayComPortsNames[0];
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (_serial_port == null)
+            {
+                try
+                {
+                    _serial_port = new SerialPort(PortComboBox.Text);
+                    _serial_port.BaudRate = 9600;
+                    _serial_port.Parity = Parity.None;
+                    _serial_port.StopBits = StopBits.One;
+                    _serial_port.DataBits = 8;
+                    _serial_port.Handshake = Handshake.None;
+                    _serial_port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                    _serial_port.Open();
+                    ConnectButton.Text = "Disconnect";
+                    SendCommand("B1\r");
+                }
+                catch (Exception ex)
+                {
+                    _serial_port = null;
+                    ConnectButton.Text = "Connect";
+                    System.Console.WriteLine(ex.Message);
+                    string error_text = "Unable to open serial port";
+                    MessageBox.Show(error_text);
+                }
+            }
+            else
+            {
+                bPA2Test = false;
+                PA2Test.Text = "PA2 OFF";
+                _serial_port.Close();
+                _serial_port = null;
+                ConnectButton.Text = "Connect";
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort serial_port = (SerialPort)sender;
+            string indata = serial_port.ReadExisting();
+            foreach (char ch in indata)
+            {
+                if (ch=='\r'||ch=='\n')
+                {
+                }
+                else
+                {
+                }
+            }
+        }
+
+        void SendCommand(string text)
+        {
+            if (_serial_port != null && _serial_port.IsOpen)
+            {
+                _serial_port.Write(text);
+            }
+        }
+
+        float Vrms = 231.48f;   //2
+        float Irms = 2.31f; //3
+        float Preal = 480.23f;  //4
+        float VA = 0;   //5
+        float QA = 12;  //6
+        float QI = 34;  //7
+        float PF = 80;   //8
+        float Temp = 37.5f;   //9
+        float PH = 33;  //10
+        float PFU = 44; //11
+        float PQF = 55; //12
+        float Watthr = 0; //13
+        int Interval = 0;
+
+        Random random = new Random();
+
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            float fVrms = 225.0f + random.Next(0, 100)/10.0f;
+            float fIrms = 0.0f + random.Next(0, 100)/40.0f;
+            float fPF = 80.0f + random.Next(0, 100)/10.0f;
+            setTextboxText(fVrms, fIrms, fPF);
+        }
+
+        void setTextboxText(float fVrms, float fIrms, float fPF)
+        {
+            if (this.InvokeRequired)
+            { 
+                this.Invoke(new IntrDelegate(SetTextboxTextSafe), new object[] { fVrms, fIrms, fPF }); 
+            }
+            else
+            {
+                SetTextboxTextSafe(fVrms, fIrms, fPF);
+            }
+        }
+
+        delegate void IntrDelegate(float fVrms, float fIrms, float fPF);
+
+        void SetTextboxTextSafe(float fVrms, float fIrms, float fPF)
+        {
+            Vrms = fVrms;
+            Vrms_textBox.Text = Vrms.ToString();
+            Irms = fIrms;
+            Irms_textBox.Text = Irms.ToString();
+            PF_textBox.Text = fPF.ToString();
+            PF = fPF/100.0f;
+            PF_textBox.Text = fPF.ToString();
+
+            VA = Vrms * Irms;
+            Preal = VA * PF;
+            Preal_textBox.Text = Preal.ToString();
+
+            Watthr += Preal / 3600.0f;
+            Watthr_textBox.Text = Watthr.ToString();
+
+            Interval += 1;
+            Interval_textBox.Text = Interval.ToString();
+
+            if (bPA2Test)
+            {
+                string PA2_data = (char)2 + "OK,1000," + Vrms.ToString() + "," + Irms.ToString() + ","
+                    + Preal.ToString() + "," + VA.ToString() + "," + QA.ToString() + "," + QI.ToString() + ","
+                    + PF.ToString() + "," + Temp.ToString() + "," + PH.ToString() + "," + PFU.ToString() + ","
+                    + PQF.ToString() + "," + Watthr.ToString() + "," + Interval.ToString() + (char)3;
+
+                SendCommand(PA2_data);
+            }
+        }
+
+        bool bPA2Test = false;
+
+        private void PA2Test_Click(object sender, EventArgs e)
+        {
+            if (_serial_port!=null)
+            {
+                if (!bPA2Test)
+                {
+                    bPA2Test = true;
+                    PA2Test.Text = "PA2 ON";
+                }
+                else
+                {
+                    bPA2Test = false;
+                    PA2Test.Text = "PA2 OFF";
+                }
+            }
+        }
+
+    }
+}
